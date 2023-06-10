24d10a846f351ef7db716e7ad54ac9f05d151859
CD <102435352+carlosdaguano@users.noreply.github.com>
2023-06-10 00:06:06 -0300

    Delete src/src directory

--- src/src/components/atividade6/Calculadora.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Calculadora = () => {
-    return(
-        <h1>Calculadora</h1>
-    );
-}
-
-export default Calculadora;
--- src/src/components/atividade6/JogoDaMemoria.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const JogoDaMemoria = () => {
-    return(
-        <h1>JogoDaMemoria</h1>
-    );
-}
-
-export default JogoDaMemoria;
