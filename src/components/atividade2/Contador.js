a32e9f1ce95f22c1792eb4636acc0e84e21679d0
CD <102435352+carlosdaguano@users.noreply.github.com>
2023-06-14 10:33:38 -0300

    Add files via upload

--- src/components/atividade2/Contador.js
+++ src/components/atividade2/Contador.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { useState } from 'react';
 
-function Button({count, onClick}){
+function Button({ operation, onClick }) {
 
-    return(
-        <button onClick={onClick}>
-            Clicado {count} vezes
-        </button>
-    );
+    if (operation === "+") {
+        return (
+            <button onClick={onClick}>+</button>
+        );
+    } else if (operation === "-") {
+        return (
+            <button onClick={onClick}>-</button>
+        )
+    }
 }
 
-function Reset({onClick}){
-    return(
+function Reset({ onClick }) {
+    return (
         <button onClick={onClick}>
             Reset
         </button>
@@ -20,33 +24,52 @@ function Reset({onClick}){
 
 
 
-function Contador(){
+function Contador() {
     const [countTotal, setCountTotal] = useState(0);
     const [count1, setCount1] = useState(0);
     const [count2, setCount2] = useState(0);
 
-    function handleClick1(){
+    function handleClick1Add() {
         setCountTotal(countTotal + 1);
         setCount1(count1 + 1);
     }
-    
-    function handleClick2(){
+    function handleClick1Sub() {
+        setCountTotal(countTotal - 1);
+        setCount1(count1 - 1);
+    }
+
+    function handleClick2Add() {
         setCountTotal(countTotal + 1);
         setCount2(count2 + 1);
     }
-    function resetcounts(){
+
+    function handleClick2Sub() {
+        setCountTotal(countTotal - 1);
+        setCount2(count2 - 1);
+    }
+    function resetcounts() {
         setCountTotal(0);
         setCount1(0);
         setCount2(0);
     }
 
-    return(
+    return (
         <div>
             <h2>Contador</h2>
             <p>{countTotal}</p>
-            <Button count={count1} onClick={handleClick1}/>
-            <Button count={count2} onClick={handleClick2}/>
-            <Reset onClick={resetcounts}/>
+            <div>
+                <h1>Valor 1:{count1}</h1>
+                <Button operation={"+"} onClick={handleClick1Add} />
+                <Button operation={"-"} onClick={handleClick1Sub} />
+            </div>
+            <div>
+                <h1>Valor 2:{count2}</h1>
+                <Button operation={"+"} onClick={handleClick2Add} />
+                <Button operation={"-"} onClick={handleClick2Sub} />
+            </div>
+
+
+            <Reset onClick={resetcounts} />
 
         </div>
     );
